c1433eca9d57ed2579fe46d80a2441309237540a
James Freeman <jfreeman@activecampaign.com>
2015-09-15 02:58:46 -0400

    Fix mis-use of ActiveCampaign name
    We purposefully are ignoring "active campaign", since we don't want to get instances of people talking about campaigns that are active.

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,9 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bActive Campaign\b/g, "ActiveCampaign");
+	v = v.replace(/\bActivecampaign\b/g,  "ActiveCampaign");
+	v = v.replace(/\bactivecampaign\b/g,  "ActiveCampaign");
 	
 	textNode.nodeValue = v;
 }
